3fbd9e03c5152e166c83a24c581d9481709a761f
Vladimir.Kasatkin <Vladimir.Kasatkin@acronis.com>
2022-04-27 12:22:54 +0300

    Add 'Group' handler'

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -9,17 +9,10 @@ import (
 	"github.com/torwald-sergesson/app-a/pkg/dto"
 )
 
-type meHandler struct{}
+type baseHandler struct{}
 
-func (h *meHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-	data := dto.User{
-		ID:   1,
-		Name: "Odin",
-		Age:  45,
-		Tags: []string{"hallo", "world", "new"},
-	}
-
-	buf, err := json.Marshal(&data)
+func (h *baseHandler) Handle(obj interface{}, w http.ResponseWriter, r *http.Request) {
+	buf, err := json.Marshal(obj)
 	if err != nil {
 		w.WriteHeader(http.StatusInternalServerError)
 		w.Write([]byte(fmt.Sprintf("%s", err)))
@@ -31,8 +24,35 @@ func (h *meHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	w.Write(buf)
 }
 
+type meHandler struct {
+	baseHandler
+}
+
+func (h *meHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	data := dto.User{
+		ID:   1,
+		Name: "Odin",
+		Age:  45,
+		Tags: []string{"hallo", "world", "new"},
+	}
+	h.Handle(&data, w, r)
+}
+
+type myGroupHandler struct {
+	baseHandler
+}
+
+func (h *myGroupHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	data := dto.Group{
+		ID:   1,
+		Name: "Asgard",
+	}
+	h.Handle(&data, w, r)
+}
+
 func main() {
 	http.Handle("/api/me", &meHandler{})
+	http.Handle("/api/group/my", &myGroupHandler{})
 
 	if err := http.ListenAndServe("localhost:8080", nil); err != nil {
 		log.Fatalf("error: %s", err)
