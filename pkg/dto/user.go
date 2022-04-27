7d0ca7b3468f9874948fd222571cf1ca731a94f1
Vladimir.Kasatkin <Vladimir.Kasatkin@acronis.com>
2022-04-27 14:01:17 +0300

    Add 'group' to 'User' DTO

--- pkg/dto/user.go
+++ pkg/dto/user.go
@@ -1,8 +1,9 @@
 package dto
 
 type User struct {
-	ID   int64    `json:"id"`
-	Name string   `json:"name"`
-	Age  int      `json:"age"`
-	Tags []string `json:"tags"`
+	ID    int64    `json:"id"`
+	Name  string   `json:"name"`
+	Age   int      `json:"age"`
+	Group Group    `json:"group"`
+	Tags  []string `json:"tags"`
 }
